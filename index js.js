f39d7dc63d3f093e29bf307d2a65cfb0efd6d717
ZYNABMostafa33 <152556337+ZYNABMostafa33@users.noreply.github.com>
2024-09-10 19:00:15 +0300

    Add files via upload

---
+++ index js.js
@@ -0,0 +1,20 @@
+// let names=['z','m','a'];
+
+// for (let i=names.length;i>=0;i--)
+//     {
+//         console.log(names[i]); 
+//     }
+
+let cars=['BMW', 'Mercedes','Honda'];
+let model=[2020,2021,2022];
+for (let i=0 ; i<cars.length;i++)
+    {
+        console.log(`Cars : ${cars[i]}`)
+        for(let j=0; j<model.length;j++)
+            {
+                console.log(`Model: ${model[j]}`)
+            }
+            console.log('---------------------')
+    } 
+
+
